b8e4270a3192d7a82ec72246e725014768f444e5
S401 <S401@S401-22>
2016-07-06 11:46:04 +0900

    0706 풀이

--- src/practice01/Game369.java
+++ src/practice01/Game369.java
@@ -3,6 +3,34 @@ package practice01;
 public class Game369 {
 
 	public static void main(String[] args) {
+		String answer = null;
+		
+		System.out.println("실행결과 : ");
+		System.out.println("-------");
+		
+		for(int i=1;i<100;i++){
+			int number = i;
+			//String -> int : Integer.parseInt(String)
+			//int -> String : String.valueOf(int)
+			String sNumber = String.valueOf(number);
+			//System.out.println(sNumber);
+			//System.out.println(sNumber.length());
 
+			for(int j=0; j<sNumber.length();j++){
+				int cnt=0;
+				char c = sNumber.charAt(j);
+				if(c=='3' || c=='6' || c=='9'){
+					cnt = cnt + 1;
+					System.out.print(number+" ");
+				}
+				if(cnt>0){
+					for(int k=cnt;k>0;k--){
+						System.out.print("짝");
+					}
+				}else{}
+			}
+
+			//char c = sNumber.charAt(0);
+		}
 	}
 }
--- src/practice01/GuessNumber.java
+++ src/practice01/GuessNumber.java
@@ -1,7 +1,51 @@
 package practice01;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessNumber {
 	public static void main(String[] args) {
+		
+		while(true){
+			int First = 1;
+			int End = 100;
+			int i=1;
+			int Ianswer;
+			Random random = new Random();
+			int number = random.nextInt(100)+1;
+			
+			System.out.println("수를 결정하였습니다. 맞추어보세요");
+			//System.out.println(number);
+			Scanner scanner = new Scanner(System.in);
+			
+			while(true){
+				System.out.println("["+First + "-" + End+"]");
+				System.out.print((i++)+">> ");
+				String answer = scanner.next();
+				
+				Ianswer = Integer.parseInt(answer);
+				
+				if(Ianswer>number){
+					System.out.println("더낮게");
+					End = Ianswer;
+				}else if(Ianswer==number){
+					System.out.println("맞았습니다.");
+					break;
+				}else{
+					System.out.println("더 높게");
+					First = Ianswer;
+				}
+			}
+			System.out.println("다시 하시겠습니까?(y,n)>>");
+			String Eanswer = scanner.next();
+			if( Eanswer.equals("n") ){
+				scanner.close();
+				System.out.println("게암을 종료합니다.");
+			    break;//종료하도록 작성한다.	
+			}else{
+				continue;
+			}
 
+		}
 	}
 }
